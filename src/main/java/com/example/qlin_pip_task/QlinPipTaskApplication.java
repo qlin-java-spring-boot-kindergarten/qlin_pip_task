dfaab9c4ba125939c4df942c1a534c014e3c098c
qlin077 <qingqing.lin@baruchcollege.edu>
2022-06-03 11:40:37 +0800

    init the project

---
+++ src/main/java/com/example/qlin_pip_task/QlinPipTaskApplication.java
@@ -0,0 +1,13 @@
+package com.example.qlin_pip_task;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QlinPipTaskApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(QlinPipTaskApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/qlin_pip_task/QlinPipTaskApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.qlin_pip_task;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QlinPipTaskApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
