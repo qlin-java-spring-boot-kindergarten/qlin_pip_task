7d0ef223dd4ce71743bd81891446d94e8f1752d6
qlin077 <qingqing.lin@baruchcollege.edu>
2022-06-04 18:50:46 +0800

    finish partial isolation test with real data testing

--- src/isolation-test/java/com/example/qlin_pip_task/controller/StudentsControllerTest.java
+++ src/isolation-test/java/com/example/qlin_pip_task/controller/StudentsControllerTest.java
@@ -1,11 +1,24 @@
 package com.example.qlin_pip_task.controller;
 
+import org.hamcrest.Matchers;
 import org.junit.jupiter.api.Test;
 
-public class StudentsControllerTest extends IsolationTest{
+public class StudentsControllerTest extends IsolationTest {
 
     @Test
-    void should_return_status_list_when_call_get_students_api(){
+    void should_return_status_list_when_call_get_students_api() {
+
+        givenRestWithAuth()
+                .when()
+                .get("/students")
+                .then()
+                .statusCode(200)
+                .body("data.size()", Matchers.is(2))
+                .body("data[0].id", Matchers.equalTo(1))
+                .body("data[0].name", Matchers.equalTo("aaaaa"))
+                .body("data[0].grade", Matchers.equalTo(3))
+                .body("data[0].classroom", Matchers.equalTo(1));
+
 
     }
 }
