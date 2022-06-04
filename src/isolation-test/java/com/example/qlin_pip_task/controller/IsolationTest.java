bf496d0ce31a42a0527d53df772f1030ba2a3561
qlin077 <qingqing.lin@baruchcollege.edu>
2022-06-04 11:39:17 +0800

    partially completed IsolationTest

--- src/isolation-test/java/com/example/qlin_pip_task/controller/IsolationTest.java
+++ src/isolation-test/java/com/example/qlin_pip_task/controller/IsolationTest.java
@@ -1,7 +1,35 @@
 package com.example.qlin_pip_task.controller;
 
+import io.restassured.RestAssured;
+import com.example.qlin_pip_task.QlinPipTaskApplication;
+import io.restassured.specification.RequestSpecification;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.server.LocalServerPort;
 import org.springframework.test.context.ActiveProfiles;
 
+import static io.restassured.RestAssured.given;
+
 @ActiveProfiles
+@SpringBootTest(classes = QlinPipTaskApplication.class, webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public abstract class IsolationTest {
+
+    @LocalServerPort
+    private transient int port;
+
+    @BeforeEach
+    public void setDefaultsForRestAssured(){
+        RestAssured.port = port;
+    }
+
+    @AfterEach
+    public void resetRestAssured(){
+        RestAssured.reset();
+    }
+
+    protected RequestSpecification givenRestWithAuth() {
+        return given();
+    }
+
 }
---
+++ src/isolation-test/java/com/example/qlin_pip_task/controller/StudentsControllerTest.java
@@ -0,0 +1,11 @@
+package com.example.qlin_pip_task.controller;
+
+import org.junit.jupiter.api.Test;
+
+public class StudentsControllerTest extends IsolationTest{
+
+    @Test
+    void should_return_status_list_when_call_get_students_api(){
+
+    }
+}
