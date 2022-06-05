84aa2f0b4061f2aa37f815cd9d4fc282f82280d8
qlin077 <qingqing.lin@baruchcollege.edu>
2022-06-05 13:55:11 +0800

    should_return_student_object_given_find_by_id

--- src/test/java/com/example/qlin_pip_task/service/ValidationServiceTest.java
+++ src/test/java/com/example/qlin_pip_task/service/ValidationServiceTest.java
@@ -11,16 +11,12 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
-import org.springframework.beans.factory.annotation.Autowired;
 
 import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
-import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyInt;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.when;
 
 
 @ExtendWith(MockitoExtension.class)
