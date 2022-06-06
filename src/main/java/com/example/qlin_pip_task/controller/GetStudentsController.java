cd8c43367af3667f7ee8e9cdbdc978f61dc5f7c0
qlin077 <qingqing.lin@baruchcollege.edu>
2022-06-06 18:52:30 +0800

    Revert "refactor: get all students api"
    This reverts commit b3769ae1

--- src/main/java/com/example/qlin_pip_task/controller/GetStudentsController.java
+++
@@ -1,9 +0,0 @@
-package com.example.qlin_pip_task.controller;
-
-import com.example.qlin_pip_task.dto.response.StudentResponse;
-import com.example.qlin_pip_task.service.StudentsService;
-import lombok.RequiredArgsConstructor;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
