d1faf50ccb7d3488289506dc861eabfb06f09302
qlin077 <qingqing.lin@baruchcollege.edu>
2022-06-08 22:03:12 +0800

    add StudentNotFoundException

---
+++ src/main/java/com/example/qlin_pip_task/exception/StudentNotFoundException.java
@@ -0,0 +1,7 @@
+package com.example.qlin_pip_task.exception;
+
+public class StudentNotFoundException extends RuntimeException {
+    public StudentNotFoundException(String messsage) {
+        super(messsage);
+    }
+}
