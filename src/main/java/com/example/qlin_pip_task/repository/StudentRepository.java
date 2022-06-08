6df442b31f2c1a68f10acffb1e8ea4139ced84d8
qlin077 <qingqing.lin@baruchcollege.edu>
2022-06-08 22:06:01 +0800

    refactor finallByName return type to List<StudentEntity>

--- src/main/java/com/example/qlin_pip_task/repository/StudentRepository.java
+++ src/main/java/com/example/qlin_pip_task/repository/StudentRepository.java
@@ -4,9 +4,11 @@ import com.example.qlin_pip_task.entity.StudentEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface StudentRepository extends JpaRepository<StudentEntity, Integer> {
 
-    StudentEntity findByName(String name);
+    List<StudentEntity> findAllByName(String name);
 
 }
