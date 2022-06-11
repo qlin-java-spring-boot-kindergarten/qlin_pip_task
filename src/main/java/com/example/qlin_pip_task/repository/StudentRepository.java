64facc8a2c20fe5db69500e46cb3140945d3fefd
qlin077 <qingqing.lin@baruchcollege.edu>
2022-06-11 16:56:32 +0800

    add PostgreSQL94CustomDialect to fix JDBC 2003 error

--- src/main/java/com/example/qlin_pip_task/repository/StudentRepository.java
+++ src/main/java/com/example/qlin_pip_task/repository/StudentRepository.java
@@ -2,6 +2,7 @@ package com.example.qlin_pip_task.repository;
 
 import com.example.qlin_pip_task.entity.StudentEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -10,5 +11,8 @@ import java.util.List;
 public interface StudentRepository extends JpaRepository<StudentEntity, Integer> {
 
     List<StudentEntity> findAllByName(String name);
+
+    @Query(value = "SELECT array_agg(student_id ORDER BY student_id asc) FROM homework GROUP BY  homework_type ORDER BY homework_type asc;", nativeQuery = true)
+    List<Integer> getHomework(Integer studentId);
 
 }
