e2db2dbc7bd750875a2d0e6d127711a8d6c27cc7
qlin077 <qingqing.lin@baruchcollege.edu>
2022-06-19 11:52:39 +0800

    add: TeacherEntity and HomeworkEntity

---
+++ src/main/java/com/example/qlin_pip_task/entity/HomeworkEntity.java
@@ -0,0 +1,47 @@
+package com.example.qlin_pip_task.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.SequenceGenerator;
+import javax.persistence.Table;
+import java.sql.Date;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "homework")
+public class HomeworkEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY, generator = "homework_id_generator")
+    @SequenceGenerator(name = "homework_id_generator", sequenceName = "homework_id_seq", allocationSize = 1)
+    private Integer id;
+    private String description;
+
+    @JoinColumn(name = "class_id")
+    private Integer class_id;
+
+    @CreationTimestamp
+    private Date createdAt;
+
+    @UpdateTimestamp
+    @Column(nullable = false, updatable = false)
+    private Date updatedAt;
+
+    @ManyToOne
+    @JoinColumn(name = "teacher_id")
+    private TeacherEntity teacherEntity;
+}
---
+++ src/main/java/com/example/qlin_pip_task/entity/TeacherEntity.java
@@ -0,0 +1,38 @@
+package com.example.qlin_pip_task.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToMany;
+import javax.persistence.SequenceGenerator;
+import javax.persistence.Table;
+import java.util.List;
+
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "teacher")
+public class TeacherEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY, generator = "teacher_id_generator")
+    @SequenceGenerator(name = "teacher_id_generator", sequenceName = "teacher_id_seq", allocationSize = 1)
+    private Integer id;
+    private String name;
+
+    @JoinColumn(name = "class_id")
+    private Integer classId;
+
+    @OneToMany(mappedBy = "teacherEntity", cascade = CascadeType.ALL)
+    private List<HomeworkEntity> homework;
+}
