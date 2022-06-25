d932f708adac75baf7e6f15d2e24913aa4c693e8
qlin077 <qingqing.lin@baruchcollege.edu>
2022-06-25 18:13:22 +0800

    format: remove unused import

--- src/main/java/com/example/qlin_pip_task/repository/StudentRepository.java
+++ src/main/java/com/example/qlin_pip_task/repository/StudentRepository.java
@@ -2,7 +2,6 @@ package com.example.qlin_pip_task.repository;
 
 import com.example.qlin_pip_task.entity.StudentEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
