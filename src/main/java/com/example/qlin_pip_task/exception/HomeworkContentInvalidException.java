09996ffa6b8baf3c6b4155722feb8bb2cf452b26
qlin077 <qingqing.lin@baruchcollege.edu>
2022-06-25 12:06:37 +0800

    refactor: extends CustomParameterException

--- src/main/java/com/example/qlin_pip_task/exception/HomeworkContentInvalidException.java
+++ src/main/java/com/example/qlin_pip_task/exception/HomeworkContentInvalidException.java
@@ -1,8 +1,7 @@
 package com.example.qlin_pip_task.exception;
 
-import java.security.InvalidParameterException;
 
-public class HomeworkContentInvalidException extends InvalidParameterException {
+public class HomeworkContentInvalidException extends CustomParameterException {
     public HomeworkContentInvalidException(String message) {
         super(message);
     }
