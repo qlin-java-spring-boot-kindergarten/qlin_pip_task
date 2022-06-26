de4fa9168a0edeb5f7374f0ddd94206f3c79e134
qlin077 <qingqing.lin@baruchcollege.edu>
2022-06-26 20:41:03 +0800

    refactor: StudentNotFoundException extends CustomResourceNotFoundException

--- src/main/java/com/example/qlin_pip_task/exception/StudentNotFoundException.java
+++ src/main/java/com/example/qlin_pip_task/exception/StudentNotFoundException.java
@@ -1,6 +1,6 @@
 package com.example.qlin_pip_task.exception;
 
-public class StudentNotFoundException extends RuntimeException {
+public class StudentNotFoundException extends CustomResourceNotFoundException {
     public StudentNotFoundException(String message) {
         super(message);
     }
